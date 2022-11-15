da1b23a05c7df230a38211ec816de2c72071b52f
winniedavis <63395082+tamuno777@users.noreply.github.com>
2022-11-15 15:16:12 +0100

    Add files via upload

---
+++ testimonial.js
@@ -0,0 +1,16 @@
+function openNav(){
+    document.getElementById("mySidenav").style.width ="100px";
+    document.getElementById("mySidenav").style.marginLeft ="0px";
+    document.getElementById("mySidenav").style.display ="block";
+    document.body.style.backgroundColor = "rgba(0,0,0,0,4)";
+
+
+}
+function closeNav(){
+    document.getElementById("mySidenav").style.width ="0";
+    document.getElementById("mySidenav").style.marginLeft ="0";
+    document.getElementById("mySidenav").style.display ="none";
+    document.body.style.backgroundColor = "white";
+
+
+}
